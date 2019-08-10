7df78a8bb53aaa01e8ead9aeefce80b289aebf46
Coreymonsta <52115121+Coreymonsta@users.noreply.github.com>
2019-08-09 22:53:20 -0400

    Create main.py

---
+++ main.py
@@ -0,0 +1,33 @@
+import matplotlib.pyplot as plt
+
+
+def calculate_gcd(x, y):
+    if x > y:
+        calculate_gcd(y, x)
+
+    for i in range(1, x + 1)[::-1]:
+        if x % i == y % i == 0:
+            return i
+
+
+def init_len_n(n):
+    seq = [None]*n
+    seq[0] = 1
+    seq[1] = 1
+
+    for i in range(2, n):
+        gcd = calculate_gcd(seq[i - 1], i)
+        seq[i] = seq[i - 1] + i + 1 if gcd == 1 else int(seq[i - 1]/gcd)
+
+    return seq
+
+print("Input how far you would like to graph the sequence (author of which appears in the Amazing Graphs - Numberphile YouTube video)")
+
+n = int(input())
+x = list(range(1, n + 1))
+y = init_len_n(n)
+
+plt.scatter(x, y)
+plt.xlabel("input value")
+plt.ylabel("sequence value")
+plt.show()
